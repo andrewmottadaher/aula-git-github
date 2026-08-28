9c311b3072bce14df9e0427017396b9fe4addb1f
Andrew Motta Daher <andrewdaher.faculdade@gmail.com>
2026-08-27 23:24:38 -0300

    Primeiro commit do projeto: classe Java Teste vazia.

---
+++ teste/src/main/java/com/aula/aula_git_github/App.java
@@ -0,0 +1,10 @@
+package com.aula.aula_git_github;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ teste/src/test/java/com/aula/aula_git_github/AppTest.java
@@ -0,0 +1,19 @@
+package com.aula.aula_git_github;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
